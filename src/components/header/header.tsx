c2a8ce356f529845b7e524e3d1916c9daf73f861
Kirill Andreev <kenokoz@yandex.ru>
2021-04-27 09:51:50 +0300

    Delete the word 'return'

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './header.scss';
 
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="header__logo">PropertyCross</div>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="header">
+    <div className="header__logo">PropertyCross</div>
+  </header>
+);
 
 export default Header;
